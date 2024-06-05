c8203ffe755f2ae0471237dc675df10479e8146e
Prathikshakj2281 <prathikshakj91@gmail.com>
2024-06-05 15:06:37 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,15 @@
+import streamlit as st
+
+# Title of the app
+st.title("Simple Streamlit App")
+
+# Define the paragraph
+paragraph = """
+Streamlit is an open-source Python library that simplifies the creation of interactive, web-based applications for data science and machine learning projects. Designed with user-friendliness in mind, Streamlit allows developers to transform data scripts into shareable web apps with minimal effort and code. 
+"""
+
+# Write the paragraph
+st.write(paragraph)
+
+
+
